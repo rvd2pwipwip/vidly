0691e2c4354dd04a1349d03a8130522628f32995
Herve Desrosiers <hdesrosiers@stingray.com>
2018-12-19 08:15:49 -0500

    added routing

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,28 @@
 import React, { Component } from "react";
+import { Route, Redirect, Switch } from "react-router-dom";
 import Movies from "./Movies";
+import NavBar from "./common/NavBar";
+import Customers from "./Customers";
+import MovieForm from "./MovieForm";
+import NotFound from "./NotFound";
+import Rentals from "./Rentals";
 
 class App extends Component {
   render() {
     return (
-      <main className="container">
-        <Movies />
-      </main>
+      <>
+        <NavBar />
+        <main className="container">
+          <Switch>
+            <Route path="/movies" component={Movies} />
+            <Route path="/customers" component={Customers} />
+            <Route path="/rentals" component={Rentals} />
+            <Route path="/not-found" component={NotFound} />
+            <Redirect exact from="/" to="/movies" />
+            <Redirect to="/not-found" />
+          </Switch>
+        </main>
+      </>
     );
   }
 }
---
+++ src/components/common/NavBar.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+class NavBar extends Component {
+  state = {};
+  render() {
+    return (
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">Vidly</nav>
+    );
+  }
+}
+
+export default NavBar;
