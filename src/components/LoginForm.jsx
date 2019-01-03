1ec57b1d49f39b8b4824db881bf67f7b643f52f5
Herve Desrosiers <hdesrosiers@stingray.com>
2019-01-03 07:41:32 -0500

    video11 basic validation implementation and display

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -8,14 +8,23 @@ class LoginForm extends Component {
   };
 
   validate = () => {
-    return { username: "Username is required." };
+    const errors = {};
+
+    const { account } = this.state;
+    if (account.username.trim() === "")
+      errors.username = "Username is required.";
+
+    if (account.password.trim() === "")
+      errors.password = "Password is required.";
+
+    return Object.keys(errors).length === 0 ? null : errors;
   };
 
   handleSubmit = e => {
     e.preventDefault();
 
     const errors = this.validate();
-    this.setState({ errors });
+    this.setState({ errors: errors || {} });
     if (errors) return;
 
     console.log("Submitted");
@@ -31,7 +40,7 @@ class LoginForm extends Component {
   };
 
   render() {
-    const { account } = this.state;
+    const { account, errors } = this.state;
     return (
       <>
         <h1>Login</h1>
@@ -41,12 +50,14 @@ class LoginForm extends Component {
             value={account.username}
             label="Username"
             onChange={this.handleChange}
+            error={errors.username}
           />
           <Input
             name="password"
             value={account.password}
             label="Password"
             onChange={this.handleChange}
+            error={errors.password}
           />
           <button className="btn btn-primary">Login</button>
         </form>
