c086994dd4737979e274f9dbbf9b0f84f86321df
Herve Desrosiers <hdesrosiers@stingray.com>
2019-01-08 07:45:16 -0500

    video18 extracting reusable form

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,10 +1,11 @@
-import React, { Component } from "react";
+import React from "react";
 import Joi from "joi-browser";
 import Input from "./common/Input";
+import Form from "./common/Form";
 
-class LoginForm extends Component {
+class LoginForm extends Form {
   state = {
-    account: { username: "", password: "" },
+    data: { username: "", password: "" },
     errors: {}
   };
 
@@ -17,68 +18,27 @@ class LoginForm extends Component {
       .label("Password")
   };
 
-  validate = () => {
-    //validate entire form
-    const options = { abortEarly: false };
-    const { error } = Joi.validate(this.state.account, this.schema, options);
-    if (!error) return null;
-    const errors = {};
-    for (let item of error.details) {
-      errors[item.path[0]] = item.message;
-    }
-    return errors;
-  };
-
-  handleSubmit = e => {
-    //validate entire form
-    e.preventDefault();
-
-    const errors = this.validate();
-    this.setState({ errors: errors || {} });
-    if (errors) return;
-
+  doSubmit = () => {
+    // Call the server
     console.log("Submitted");
   };
 
-  validateProperty = ({ name, value }) => {
-    //validate each input
-    //destructured input.name and input.value
-    const obj = { [name]: value };
-    const schema = { [name]: this.schema[name] };
-    const { error } = Joi.validate(obj, schema);
-    return error ? error.details[0].message : null;
-  };
-
-  handleChange = ({ currentTarget: input }) => {
-    const errors = { ...this.state.errors };
-    const errorMessage = this.validateProperty(input); //pass which input to validate
-
-    if (errorMessage) errors[input.name] = errorMessage;
-    else delete errors[input.name];
-    //copy state
-    const account = { ...this.state.account };
-    //update username
-    account[input.name] = input.value;
-    //update state of both account and errors
-    this.setState({ account, errors });
-  };
-
   render() {
-    const { account, errors } = this.state;
+    const { data, errors } = this.state;
     return (
       <>
         <h1>Login</h1>
         <form onSubmit={this.handleSubmit}>
           <Input
             name="username"
-            value={account.username}
+            value={data.username}
             label="Username"
             onChange={this.handleChange}
             error={errors.username}
           />
           <Input
             name="password"
-            value={account.password}
+            value={data.password}
             label="Password"
             onChange={this.handleChange}
             error={errors.password}
---
+++ src/components/common/Form.jsx
@@ -0,0 +1,56 @@
+import { Component } from "react";
+import Joi from "joi-browser";
+
+class Form extends Component {
+  state = {
+    data: {},
+    errors: {}
+  };
+
+  validate = () => {
+    //validate entire form
+    const options = { abortEarly: false };
+    const { error } = Joi.validate(this.state.data, this.schema, options);
+    if (!error) return null;
+    const errors = {};
+    for (let item of error.details) {
+      errors[item.path[0]] = item.message;
+    }
+    return errors;
+  };
+
+  validateProperty = ({ name, value }) => {
+    //validate each input
+    //destructured input.name and input.value
+    const obj = { [name]: value };
+    const schema = { [name]: this.schema[name] };
+    const { error } = Joi.validate(obj, schema);
+    return error ? error.details[0].message : null;
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+
+    const errors = this.validate();
+    this.setState({ errors: errors || {} });
+    if (errors) return;
+
+    this.doSubmit();
+  };
+
+  handleChange = ({ currentTarget: input }) => {
+    const errors = { ...this.state.errors };
+    const errorMessage = this.validateProperty(input); //pass which input to validate
+
+    if (errorMessage) errors[input.name] = errorMessage;
+    else delete errors[input.name];
+    //copy state
+    const data = { ...this.state.data };
+    //update username
+    data[input.name] = input.value;
+    //update state of both data and errors
+    this.setState({ data, errors });
+  };
+}
+
+export default Form;
